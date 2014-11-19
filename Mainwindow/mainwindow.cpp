e6a77e2a844896ae0215f9b96373941fbed93500
matla722 <mathias@Mathias-iMac-3.local>
2014-11-19 02:06:21 +0100

    Visar information om film när man klickar på titel

--- Mainwindow/mainwindow.cpp
+++ Mainwindow/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QAbstractItemView>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -25,38 +26,13 @@ void MainWindow::on_actionQuit_triggered()
 
 void MainWindow::on_pushButton_info_edit_clicked()
 {
-   QString movie("Bad boys 2");
-    // movie is used to test textbrowser (HTML) module.
-
-
-
-    ui->textBrowser_info->setHtml
-    (
-
-        "<html><head><meta name='qrichtext' content='1' /><style type='text/css'>"
-        "p, li { white-space: pre-wrap; }"
-        "</style></head><body style=' font-family:'.Helvetica Neue DeskInterface'; font-size:13pt; font-weight:400; font-style:normal;'>"
-        "<p style='-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;'><br /></p>"
-        "<p align='center' style=' margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;'>"
-     //   + movieModel->record(0).value(MovieModel::Title).toString() +
-        "</p>"
-        "<p align='center' style=' margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;'>"
-   //     "Playtime: " + movieModel->record(0).value(MovieModel::PlayTime).toString() +
-        ". "
-        "Action."
-        "</p>"
-        "<p align='center' style='-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;'><br /></p>"
-        "<p style=' margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;'>Information about movie. aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa </p></body></html>"
-    );
-
-
-
+    ;
 }
 
 void MainWindow::on_pushButton_movies_add_clicked()
 {
-    movieModel->insertMovie("Avatar", 123, 11, "ebebwen", "Adventure", 2009);
-    movieModel->insertMovie("Bad Boys 2", 456, 11, "ebebwen", "Adventure", 2009);
+    movieModel->insertMovie("Avatar", 123, 11, "Handlar om blue figures.", "Adventure", 2009);
+    movieModel->insertMovie("Bad Boys 2", 456, 11, "The boys are back in town. Watch out. tjalalalala mmmmm mm mm mmmmm mm mm mmmmm mm", "Drama", 2009);
     movieModel->submitAll();
     qDebug() << movieModel->record(0).value(MovieModel::AgeLimit).toInt();
 }
@@ -78,18 +54,48 @@ void MainWindow::on_listView_movies_clicked(const QModelIndex &index)
         "p, li { white-space: pre-wrap; }"
         "</style></head><body style=' font-family:'.Helvetica Neue DeskInterface'; font-size:13pt; font-weight:400; font-style:normal;'>"
         "<p style='-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;'><br /></p>"
-        "<p align='center' style=' margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;'>"
+        "<p align='center' style=' margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;'> <b>"
         + movieModel->record(ui->listView_movies->selectionModel()->selectedIndexes().first().row()).value(MovieModel::Title).toString() +
-        "</p>"
+        "</b> </p>"
         "<p align='center' style=' margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;'>"
-        "Playtime: " + movieModel->record(ui->listView_movies->selectionModel()->selectedIndexes().first().row()).value(MovieModel::PlayTime).toString() +
-        ". "
-        "Action."
+        + movieModel->record(ui->listView_movies->selectionModel()->selectedIndexes().first().row()).value(MovieModel::PlayTime).toString() +
+        " min. "
+        + movieModel->record(ui->listView_movies->selectionModel()->selectedIndexes().first().row()).value(MovieModel::Genre).toString() +
+        "."
         "</p>"
         "<p align='center' style='-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;'><br /></p>"
-        "<p style=' margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;'>Information about movie. aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa  aaaa aaa aaaa aaa aaaa </p></body></html>"
+        "<p style=' margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;'>"
+        + movieModel->record(ui->listView_movies->selectionModel()->selectedIndexes().first().row()).value(MovieModel::Description).toString() +
+        "</p></body></html>"
     );
-
 }
 
 
+
+void MainWindow::on_listView_movies_activated(const QModelIndex &index)
+{
+    qDebug() << "activated used";
+
+    ui->textBrowser_info->setHtml
+    (
+
+        "<html><head><meta name='qrichtext' content='1' /><style type='text/css'>"
+        "p, li { white-space: pre-wrap; }"
+        "</style></head><body style=' font-family:'.Helvetica Neue DeskInterface'; font-size:13pt; font-weight:400; font-style:normal;'>"
+        "<p style='-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;'><br /></p>"
+        "<p align='center' style=' margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;'> <b>"
+        + movieModel->record(ui->listView_movies->selectionModel()->selectedIndexes().first().row()).value(MovieModel::Title).toString() +
+        "</b> </p>"
+        "<p align='center' style=' margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;'>"
+        + movieModel->record(ui->listView_movies->selectionModel()->selectedIndexes().first().row()).value(MovieModel::PlayTime).toString() +
+        " min. "
+        + movieModel->record(ui->listView_movies->selectionModel()->selectedIndexes().first().row()).value(MovieModel::Genre).toString() +
+        "."
+        "</p>"
+        "<p align='center' style='-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;'><br /></p>"
+        "<p style=' margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;'>"
+        + movieModel->record(ui->listView_movies->selectionModel()->selectedIndexes().first().row()).value(MovieModel::Description).toString() +
+        "</p></body></html>"
+    );
+
+}
